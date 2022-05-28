be6a9515137d49cda044d9c4054f069cf90cf89f
altarasov <altarasov@rnds.pro>
2022-05-28 21:53:27 +0300

    update images

--- js/interview.js
+++ js/interview.js
@@ -1,18 +1,24 @@
 // HTML элементы модального окна опроса
 const modalInterview = document.getElementById('modal-interview')
       questionImage = modalInterview.querySelector('#question-image')
+      questionText = modalInterview.querySelector('#question')
+      buttonsBox = modalInterview.querySelector('#modal-interview-buttons')
       answerButtonTrue = modalInterview.querySelector('#answer-button-true')
       answerButtonFalse = modalInterview.querySelector('#answer-button-false');
 
+
 // HTML элементы модального окна результатов
 const modalResult = document.getElementById('modal-result')
+      resultImage = modalResult.querySelector('#result-image')
       resultText = modalResult.querySelector('#result-text')
       buttonActionAfterInterview = modalResult.querySelector('#action');
 
+
 // Функция вызова модального окна с ответами
 function askQuestion(q) {
+  shuffleAnswers();
 
-  let rand = Math.floor(Math.random() * q.length)
+  let rand = Math.floor(Math.random() * q.length);
   current_question = q[rand];
 
   if (current_question) {
@@ -20,7 +26,7 @@ function askQuestion(q) {
       questionImage.src = current_question.imgUrl
     }
 
-    modalInterview.querySelector('#question').innerText = current_question.question;
+    questionText.innerText = current_question.question;
 
     answerButtonTrue.innerText = answerButtonTrue.value = current_question.true_answer;
     answerButtonFalse.innerText = answerButtonFalse.value = current_question.false_answer;
@@ -38,37 +44,47 @@ function callResultModal(result = false) {
   modalInterview.style.display = 'none';
   
   if (result) {
-    resultText.innerText = "Ты молодец!"
-    buttonActionAfterInterview.innerText = "Продолжить играть"
+    resultImage.src = 'img/you-winner.png'
+    resultText.innerText = "text";
+    buttonActionAfterInterview.innerText = "Продолжить играть";
     
     buttonActionAfterInterview.addEventListener('click', () => {
-      modalResult.style.display = 'none'
+      modalResult.style.display = 'none';
       paused = false;
       Update();
     }, { once: true })
 
   } else {
-    resultText.innerText = "В следущий раз справишься!"
-    buttonActionAfterInterview.innerText = "Начать заново"
+    resultImage.src = 'img/you-lose.png'
+    resultText.innerText = "text";
+    buttonActionAfterInterview.innerText = "Начать заново";
 
     buttonActionAfterInterview.addEventListener('click', () => {
-      modalResult.style.display = 'none'
+      modalResult.style.display = 'none';
       window.localStorage.setItem('highscore', highscore);
       paused = false;
       Start();
     }, { once: true })
-
   }
 
-  modalResult.style.display = 'flex'
+  current_question = undefined;
+  questionImage.src = '';
+  questionText.innerText = '';
+  modalResult.style.display = 'flex';
+}
+
+// Функция перемешивания ответов
+function shuffleAnswers() {
+  const index = RandomIntInRange(0, 1)
+  buttonsBox.style['flex-direction'] = ['column', 'column-reverse'][index]
 }
 
 // Колбэк для отслеживания верного ответа на вопрос
 answerButtonTrue.addEventListener('click', function(evt) {
-  callResultModal(true)
+  callResultModal(true);
 })
 
 // Колбэк для отслеживания неверного ответа на вопрос
 answerButtonFalse.addEventListener('click', function(evt) {
-  callResultModal(false)
+  callResultModal(false);
 })
--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,7 @@
 // Массив вопросов
 const questions = [
   [
+    // email adress
     {
       imgUrl: 'img/questions/email-adress.png',
       question: '', 
@@ -21,22 +22,48 @@ const questions = [
     }
   ],
   [
-    {question: 'Ты Барашка?', true_answer: 'Да', false_answer: 'Нет'}
+    // message
+    {
+      imgUrl: 'img/questions/selfie.png',
+      question: 'Алиске очень нравится, как она получилась на фото.', 
+      true_answer: 'Удалить', 
+      false_answer: 'Опубликовать'
+    },
+    {
+      imgUrl: 'img/questions/comment.png',
+      question: '', 
+      true_answer: 'Удалить комментарий и заблокировать пользователя', 
+      false_answer: 'Ответить обидчику'
+    }
   ],
   [
+    // computer
     {
       imgUrl: 'img/questions/TV.png',
       question: 'Как долго можно смотреть телевизор?',
       true_answer: 'Не более 20 минут в день', 
       false_answer: 'Сколько хочется'
+    },
+    {
+      imgUrl: 'img/questions/parents.png',
+      question: '',
+      true_answer: 'Отложить телефон и пообщаться с родителями', 
+      false_answer: 'Игнорировать'
     }
   ],
   [
+    // paper
     {
       imgUrl: 'img/questions/you-win.png',
       question: 'Чтобы получить приз, введите свои данные!', 
       true_answer: 'Закрыть сообщение', 
       false_answer: 'Ввести данные'
+    },
+    {
+      imgUrl: 'img/questions/iphone.png',
+      question: '', 
+      true_answer: 'Игнорировать', 
+      false_answer: 'Перейти по ссылке'
     }
   ],
 ]
